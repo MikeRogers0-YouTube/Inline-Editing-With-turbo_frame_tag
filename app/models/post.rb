b709876dc02592c9894f55df28611014278c68ce
Mike Rogers <me@mikerogers.io>
2021-01-01 16:26:58 +0000

    Making the post inline editable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  broadcasts_to ->(post) { :posts }
+
+  after_update_commit :update_show
+
+  private
+
+  def update_show
+    broadcast_replace_to([self, :view], target: self, partial: 'posts/show')
+  end
 end
